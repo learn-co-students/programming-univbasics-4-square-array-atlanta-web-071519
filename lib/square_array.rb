a4771094e3b1ae6d30a7b08993ee52605b5f928d
John Odom <joodom91@gmail.com>
2019-06-15 12:31:43 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+def square_array(numbers) #creating a method
+  i=0 #setting the index equal to 0
+  new_array = []
+
+  while i<numbers.length do #looping through the index length
+  new_array[i] = numbers[i]*numbers[i] #squaring the index number
+  i+=1 #increments the index by 1
+  end
+  return new_array
 end
